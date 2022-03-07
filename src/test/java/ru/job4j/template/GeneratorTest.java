3f64b160a375710f3e6acf1eac44ac11a22aee5b
Carcharodon80 <karna241280@gmail.com>
2022-03-08 01:30:54 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.template;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GeneratorTest {
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNoKey() {
+        Generator generator = new StarWarsGenerator();
+        String template = ("${name}, I am your ${subject}!");
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Luke");
+        generator.produce(template, map);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenExtraKey() {
+        Generator generator = new StarWarsGenerator();
+        String template = ("${name}, I am your ${subject}!");
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Luke");
+        map.put("subject", "father");
+        map.put("nanny", "Mary Poppins");
+        generator.produce(template, map);
+    }
+}
