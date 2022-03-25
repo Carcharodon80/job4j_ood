a1ec225f2b0e18571bb5ecb0a77d0d358f3dc113
Carcharodon80 <karna241280@gmail.com>
2022-03-25 16:39:01 +0300

    temp

---
+++ src/main/java/ru/job4j/design/srp/lsp/Trash.java
@@ -0,0 +1,18 @@
+package ru.job4j.design.srp.lsp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trash implements Storage{
+    List<Food> foodList = new ArrayList<>();
+
+    @Override
+    public void add(Food food) {
+        foodList.add(food);
+    }
+
+    @Override
+    public List<Food> getFoodList() {
+        return foodList;
+    }
+}
