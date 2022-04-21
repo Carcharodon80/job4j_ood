27c74da71ed0a61461eaab4a7df3f56086db9362
Carcharodon80 <karna241280@gmail.com>
2022-04-21 11:41:56 +0300

    2. Парковка машин [#853 #287907]

--- src/main/java/ru/job4j/design/lsp/parking/CarParking.java
+++ src/main/java/ru/job4j/design/lsp/parking/CarParking.java
@@ -10,14 +10,16 @@ import java.util.List;
  * или несколько легковых мест (зависит от размера грузовика)
  */
 public class CarParking implements Parking {
-    private final List<Car> sedanCars = new ArrayList<>();
-    private final List<Car> truckPlaces = new ArrayList<>();
+    private final List<Car> sedanCars;
+    private final List<Car> truckPlaces;
     private int freeSedanPlaces;
     private int freeTruckPlaces;
 
     public CarParking(int countSedanPlaces, int countTruckPlaces) {
         this.freeSedanPlaces = countSedanPlaces;
         this.freeTruckPlaces = countTruckPlaces;
+        sedanCars = new ArrayList<>(countSedanPlaces);
+        truckPlaces = new ArrayList<>(countTruckPlaces);
     }
 
     /**
@@ -28,14 +30,10 @@ public class CarParking implements Parking {
     @Override
     public boolean addCar(Car car) {
         boolean rsl = false;
-        if (car.getSize() == Sedan.SIZE) {
-            if (freeSedanPlaces > 0) {
-                rsl = parkSedan(car);
-            }
-        } else {
-            if (freeTruckPlaces > 0 || freeSedanPlaces >= car.getSize()) {
-                rsl = parkTruck(car);
-            }
+        if (car.getSize() == Sedan.SIZE && freeSedanPlaces > 0) {
+            rsl = parkSedan(car);
+        } else if ((car.getSize() != Sedan.SIZE) && (freeTruckPlaces > 0 || freeSedanPlaces >= car.getSize())) {
+            rsl = parkTruck(car);
         }
         return rsl;
     }
@@ -46,11 +44,16 @@ public class CarParking implements Parking {
     }
 
     private boolean parkTruck(Car car) {
+        boolean isParking = false;
         if (freeTruckPlaces > 0) {
             freeTruckPlaces--;
+            isParking = truckPlaces.add(car);
         } else if (freeSedanPlaces >= car.getSize()) {
             freeSedanPlaces -= car.getSize();
+            for (int i = 0; i < car.getSize(); i++) {
+                isParking = sedanCars.add(car);
+            }
         }
-        return truckPlaces.add(car);
+        return isParking;
     }
 }
